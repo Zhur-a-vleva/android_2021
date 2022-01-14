4fea23b2ce735d71bd6a27f458f9f403535da7ed
Zhur.a.vleva <Zhur.a.vleva.Darya21@gmail.com>
2022-01-14 13:52:27 +0300

    [ADDED]: second simple example with timer

--- CoolTimer/app/src/main/java/com/example/cooltimer/MainActivity.kt
+++ CoolTimer/app/src/main/java/com/example/cooltimer/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.cooltimer
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.os.Handler
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -10,14 +11,28 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
-        val handler = Handler()
-        val runnable = object : Runnable {
-            override fun run() {
-                Log.d("Runnable: ", "Two seconds are passed")
-                handler.postDelayed(this, 2000)
+
+//        val handler = Handler()
+//        val runnable = object : Runnable {
+//            override fun run() {
+//                Log.d("Runnable: ", "Two seconds are passed")
+//                handler.postDelayed(this, 2000)
+//            }
+//        }
+//        handler.post(runnable)
+
+        val countDownTimer = object: CountDownTimer(11000, 1000) {
+
+            override fun onTick(millisUntilFinished: Long) {
+                Log.d("Timer: ", (millisUntilFinished/1000).toString() + " seconds left")
             }
+
+            override fun onFinish() {
+                Log.d("Timer: ", "Finish!")
+            }
+
         }
-        handler.post(runnable)
+
+        countDownTimer.start()
     }
 }
